ca8941a5d6d0e2a74721f09c6706f607601d4293
Shounak Ghosh <crazykavlogs@gmail.com>
2023-04-13 00:28:48 +0530

    created the continue screen UI

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaView, View } from "react-native";
+import ContinueScreen from "./screens/ContinueScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View className="flex-1 bg-[#1a1a1a]">
+      <SafeAreaView className="flex-1 mt-10">
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Continue">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Continue" component={ContinueScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </View>
   );
 }
---
+++ screens/ContinueScreen.js
@@ -0,0 +1,84 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+
+const ContinueScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  return !fontsLoaded ? (
+    <></>
+  ) : (
+    <View className="flex bg-[#1a1a1a] flex-grow">
+      <View className="flex mx-auto mt-5">
+        <Image
+          source={require("../assets/continue-pic.png")}
+          style={{ width: 250, height: 250 }}
+        />
+      </View>
+      <View className="flex px-[35] pt-5 mt-2">
+        <Text
+          className="text-white text-4xl mt-1"
+          style={{ fontFamily: "Inter_900Black" }}
+        >
+          The only
+        </Text>
+        <Text
+          className="text-white text-4xl mt-1"
+          style={{ fontFamily: "Inter_900Black" }}
+        >
+          finance
+        </Text>
+        <Text
+          className="text-white text-4xl mt-1"
+          style={{ fontFamily: "Inter_900Black" }}
+        >
+          app you need
+        </Text>
+      </View>
+      <View className="flex flex-row px-[35] pt-16 w-auto">
+        <TouchableOpacity className="flex-grow flex bg-[#1e90ff] text-white font-bold py-5 px-4 rounded-full">
+          <Text
+            className="text-white mx-auto"
+            style={{ fontFamily: "Inter_900Black" }}
+          >
+            Sign in with E-mail
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex flex-row w-auto px-[35] mt-6">
+        <TouchableOpacity className="flex-1 mr-3 flex-grow flex bg-[#1a1a1a] text-white font-bold border-[#ffffff] border-2 py-5 px-4 rounded-full">
+          <Text
+            className="text-white mx-auto"
+            style={{ fontFamily: "Inter_900Black" }}
+          >
+            Google
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 ml-3 flex-grow flex bg-[#1a1a1a] text-white font-bold border-[#ffffff] border-2  py-5 px-4 rounded-full">
+          <Text
+            className="text-white mx-auto"
+            style={{ fontFamily: "Inter_900Black" }}
+          >
+            Apple ID
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex flex-row px-[35] w-auto mt-8">
+        <Text className="text-white text-xs mx-auto">
+          By continuing, you agree to the Terms and Conditions
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ContinueScreen;
