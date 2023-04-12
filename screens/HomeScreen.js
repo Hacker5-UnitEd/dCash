75334ce2022b73aa773a13c1f41ed3e8f0345cae
Shounak Ghosh <crazykavlogs@gmail.com>
2023-04-12 12:57:33 +0530

    An initial commit.

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="mt-10 bg-red-800">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
