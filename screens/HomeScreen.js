b59db87dbdda78d741fe04d94bade006e914a16c
Shounak Ghosh <crazykavlogs@gmail.com>
2023-04-15 20:53:17 +0530

    changed the default homescreen a bit (ps: made this commit from my phone :-P)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,23 @@
 import { View, Text } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-
+ 
   useLayoutEffect(() => {
-    navigation.setOptions({ headerShown: false });
+        navigation.setOptions({ headerShown: false });
   }, []);
 
-  return (
-    <View className="bg-red-500">
+  let [fontsLoaded] = useFonts({
+        Inter_900Black,
+  });
+
+  return !fonstsLoaded ? (
+    <></>
+  ) : (
+    <View className="flex bg-[#1a1a1a] flex-grow">
       <Text className="mx-auto">HomeScreen</Text>
     </View>
   );
